a1ef52a6575eca99d2fd7dcb576b6b83fd59b331
Patrick Girnghuber <p.girnghuber@yahoo.de>
2022-03-17 22:44:04 +0100

    added class for ALL pricetags and fixed display

--- content.js
+++ content.js
@@ -1,93 +1,88 @@
-
 const myStyleElement = document.createElement("style");
-const cssRuleElement = `span[class*="price"]{
-color: transparent!important;
-text-shadow: rgba(0, 0, 0, 0.8) 10px 0px 10px !important;
-border-style: dotted;
-border-color: #f23838;
-}
+const cssRuleElement = `span[class*="price"],
+span[class="tmm-olp-links"] *
+{
+    color: transparent !important;
+    user-select: none;
+  }
 `;
 
 let extensionEnabled = true;
 
 const setEnabled = (boolean) => {
-    extensionEnabled = boolean;
-}
+  extensionEnabled = boolean;
+};
 
 const overlay_on = () => {
-    overlay_set('block');
-}
+  overlay_set("block");
+};
 
 const overlay_off = () => {
-    overlay_set('none');
-}
+  overlay_set("none");
+};
 
 const overlay_set = (displayStyle) => {
-    createOverlay();
-    if (document.getElementById("hide-overlay")) {
-        document.getElementById("hide-overlay").style.display = displayStyle;
-    }
-}
+  createOverlay();
+  if (document.getElementById("hide-overlay")) {
+    document.getElementById("hide-overlay").style.display = displayStyle;
+  }
+};
 
 const hide_price = () => {
-    document.head.appendChild(myStyleElement);
-    myStyleElement.sheet.insertRule(cssRuleElement, 0);
-}
+  document.head.appendChild(myStyleElement);
+  myStyleElement.sheet.insertRule(cssRuleElement, 0);
+};
 
 const show_price = () => {
-    document.head.appendChild(myStyleElement);
-    myStyleElement.sheet.deleteRule(0)
-}
+  document.head.appendChild(myStyleElement);
+  myStyleElement.sheet.deleteRule(0);
+};
 
 const createOverlay = () => {
-    if (!document.getElementById("hide-overlay") && document.childNodes[1]) {
-        var newDiv = document.createElement("div");
-        newDiv.setAttribute("id", "hide-overlay");
-        document.childNodes[1].appendChild(newDiv)
-    }
-}
+  if (!document.getElementById("hide-overlay") && document.childNodes[1]) {
+    var newDiv = document.createElement("div");
+    newDiv.setAttribute("id", "hide-overlay");
+    document.childNodes[1].appendChild(newDiv);
+  }
+};
 
 /**
  * Content to Background communication
  */
 chrome.runtime.sendMessage({ provide_enabled: "extensionEnabled" }, function (response) {
-    extensionEnabled = response.enabled;
+  extensionEnabled = response.enabled;
 });
 
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        setEnabled(request.enabled);
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  setEnabled(request.enabled);
 
-        if (request.enabled == true) {
-            sendResponse({ setting: "enabled" });
-            createOverlay();
-            hide_price();
-        }
-        else {
-            sendResponse({ setting: "disabled" });
-            overlay_off();
-            show_price();
-        }
-    });
+  if (request.enabled == true) {
+    sendResponse({ setting: "enabled" });
+    createOverlay();
+    hide_price();
+  } else {
+    sendResponse({ setting: "disabled" });
+    overlay_off();
+    show_price();
+  }
+});
 
 /**
  * Extension hide/show
  */
 createOverlay();
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    if (extensionEnabled) {
-        hide_price();
-        overlay_off();
-    }
+document.addEventListener("DOMContentLoaded", (event) => {
+  if (extensionEnabled) {
+    hide_price();
+    overlay_off();
+  }
 });
 
-document.addEventListener('readystatechange', (event) => {
-    if (extensionEnabled) {
-        if (document.readyState != 'complete') {
-            overlay_on();
-        }
+document.addEventListener("readystatechange", (event) => {
+  if (extensionEnabled) {
+    if (document.readyState != "complete") {
+      overlay_on();
     }
+  }
 });
-
-
